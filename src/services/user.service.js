a9fa3b978b1f71c2d77bed09e0bf5ddc4d84d69b
Akash Kumar <akash.199777@gmail.com>
2021-10-30 12:17:40 +0530

    https url

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import authHeader from './auth-header';
 
 const API_URL = 'https://frozen-river-95471.herokuapp.com/';
-const D_API_URL = 'http://localhost:7000/';
+// const D_API_URL = 'http://localhost:7000/';
+const D_API_URL = 'https://covibuddy.herokuapp.com/';
 
 const getDoctors = () => axios.get(`${API_URL}api/v1/doctors`, { headers: authHeader() });
 const getDoctor = id => axios.get(`${API_URL}api/v1/doctors/${id}`, { headers: authHeader() });
